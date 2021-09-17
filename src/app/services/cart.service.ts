61fdc0dbe4d7534415942a5367f96b2d95e59076
uttamgithub <uttam.panta@gmail.com>
2021-09-17 19:06:13 -0400

    2 files modified

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -24,6 +24,8 @@ export class CartService {
   }
 
   getShippingPrices() {
-    this.http.get<{ type: string; price: number }[]>('assets/shipping.json');
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 }
